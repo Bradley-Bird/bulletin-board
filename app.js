c2c148effc469bcb3576d87f07fb00197b387786
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-14 19:56:20 -0800

    connected main page to supabase

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // import functions and grab DOM elements
-
+const board = document.getElementById('board');
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
---
+++ fetch-utils.js
@@ -0,0 +1,15 @@
+const SUPABASE_URL = `https://rkpkbgcxtxmmqwaozrit.supabase.co`;
+const SUPABASE_KEY = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrcGtiZ2N4dHhtbXF3YW96cml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0NzksImV4cCI6MTk1OTkxNzQ3OX0.BlZaNNVLhHKpWOLgA-78IfDScamHmyZyr18toNO8npQ`;
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
+
+export async function getPosts() {
+    const resp = await client.from('bulletin-board').select('*');
+    console.log(resp);
+    return checkError(resp);
+}
+console.log('hello', await getPosts());
